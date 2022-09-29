18f94b32497d209a537e126c11e03d02c3221584
Karim Mousa <karimmousa202@gmail.comgit config --global --replace-all user.email karimmousa202@gmail.com>
2022-09-29 11:35:19 +0100

    m

--- primes.py
+++ primes.py
@@ -3,4 +3,12 @@
 
 def primes(number_of_primes):
     list = []
+    count = 1
+    while len(list) <= number_of_primes:
+        for i in range (2,count):
+            if count % i != 0:
+                list.append(count)
+
+        count +=1
+
     return list
